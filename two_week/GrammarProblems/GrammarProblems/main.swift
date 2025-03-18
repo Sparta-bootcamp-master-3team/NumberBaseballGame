4161b4835480d773d5ee4b079ff52f13551dd616
Sn8Ch0 <whtjsdn98@gmail.com>
2025-03-18 16:11:04 +0900

    feat: Problem2 구현

--- two_week/GrammarProblems/GrammarProblems/main.swift
+++ two_week/GrammarProblems/GrammarProblems/main.swift
@@ -7,6 +7,9 @@
 
 import Foundation
 
+var inputNumbers = [1, 2, 3, 4, 5]
+var inputWords = ["가", "나", "다", "라", "마"]
+
 // MARK: P1
 let sum: (Int, Int) -> String = { (num1, num2) in
     return "두 수의합은 \(num1 + num2) 입니다."
@@ -19,3 +22,24 @@ func calculate(closure: (Int, Int) -> String) {
 print(sum(10, 20))
 
 calculate(closure: sum)
+
+// MARK: P2
+var result = inputNumbers.map { String($0) }
+print(result)
+
+var input = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+var output = input.filter { $0 % 2 == 0 }.map { String($0) }
+print(output)
+
+func myMap(_ array: [Int], _ conversion: (Int) -> String) -> [String] {
+    var myResult = [String]()
+    
+    for i in array {
+        myResult.append(conversion(i))
+    }
+    
+    return myResult
+}
+
+let myConversion = myMap(inputNumbers) { String($0) }
+print(myConversion)
