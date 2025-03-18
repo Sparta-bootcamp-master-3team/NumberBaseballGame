cece4de0fb349feae59e1b94cf886d3c14b8b0cc
Sn8Ch0 <whtjsdn98@gmail.com>
2025-03-18 17:16:19 +0900

    feat: 도전 문제 구현

--- two_week/GrammarProblems/GrammarProblems/main.swift
+++ two_week/GrammarProblems/GrammarProblems/main.swift
@@ -98,3 +98,58 @@ func d<T: Numeric>(something: [T]) -> [T] {
 
 let resultD = d(something: inputNumbers)
 print(resultD)
+
+// MARK: Challenge Problem
+class Engine { }
+class ElectricEngine: Engine { }
+class HybridEngine: Engine { }
+
+class Car {
+    let brand: String
+    let model: String
+    let year: String
+    var engine: Engine
+    
+    init(brand: String, model: String, year: String, engine: Engine) {
+        self.brand = brand
+        self.model = model
+        self.year = year
+        self.engine = engine
+    }
+    
+    func drive() {
+        print("\(brand) \(model) 주행 중...")
+    }
+    
+    func stop() {
+        print("\(brand) \(model) 멈춤")
+    }
+    
+    func charge() {
+        print("\(brand) \(model) 충전 중..")
+    }
+    
+    func refuel() {
+        print("\(brand) \(model) 주유 중..")
+    }
+}
+
+class ElectricCar: Car {
+    init(brand: String, model: String, year: String, engine: ElectricEngine) {
+        super.init(brand: brand, model: model, year: year, engine: engine)
+    }
+}
+
+class HybridCar: Car {
+    init(brand: String, model: String, year: String, engine: HybridEngine) {
+        super.init(brand: brand, model: model, year: year, engine: engine)
+    }
+    
+    func switchEngine(to otherEngine: Engine) {
+        self.engine = otherEngine
+    }
+}
+
+let myCar = HybridCar(brand: "KIA", model: "K5", year: "2015", engine: HybridEngine())
+myCar.drive()
+
